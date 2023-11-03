baf4c07ba519a6e590c0df8df3469f1fae6ce1a7
Mirishkin Aleksandr <mirishkinaleksandr@gmail.com>
2023-11-03 11:34:25 +0300

    add func.h and func.cpp

---
+++ calculus/func.cpp
@@ -0,0 +1,21 @@
+#include "func.h"
+
+double Add(double, double)
+{
+    return 0;
+}
+
+double Sub(double, double)
+{
+    return 0;
+}
+
+double Mul(double, double)
+{
+    return 0;
+}
+
+double Div(double, double)
+{
+    return 0;
+}
