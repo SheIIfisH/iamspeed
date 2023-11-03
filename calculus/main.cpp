db23652b7463629367077bb09a15d26c8fc7bba2
Mirishkin Aleksandr <mirishkinaleksandr@gmail.com>
2023-11-03 15:49:19 +0300

    Edit comment

--- calculus/main.cpp
+++ calculus/main.cpp
@@ -5,19 +5,13 @@ using namespace std;
 
 int main()
 {
-    // Calculus
-<<<<<<< HEAD
+    // Calculus 1.1
     double a, b;
     cout << "Enter numbers: " << endl;
     cout << "a = "; cin >> a;
     cout << "b = "; cin >> b;
     cout << endl;
 
-    cout << Add(a, b) << endl;
-    cout << Sub(a, b) << endl;
-    cout << Mul(a, b) << endl;
-    cout << Div(a, b) << endl;
-=======
     cout << "Choose operator:" << endl;
     cout << " + - add\n - - sub\n * - mul\n / - div\n\n";
     cout << ">> ";
@@ -27,23 +21,21 @@ int main()
 
     switch (k){
     case '+':
-        cout << Add(5, 2) << endl;
+        cout << Add(a, b) << endl;
         break;
     case '-':
-        cout << Sub(5, 2) << endl;
+        cout << Sub(a, b) << endl;
         break;
     case '*':
-        cout << Mul(5, 2) << endl;
+        cout << Mul(a, b) << endl;
         break;
     case '/':
-        cout << Div(5, 2) << endl;
+        cout << Div(a, b) << endl;
         break;
     default:
-        cout << Add(5, 2) << endl;
+        cout << Add(a, b) << endl;
     }
 
-
->>>>>>> abfa0f0e0613e3cce1a979c3ff75ab4e5950e4c4
     return 0;
 }
 
